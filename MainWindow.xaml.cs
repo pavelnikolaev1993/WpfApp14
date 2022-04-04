c9d7d831460eb4288919ce20fc0b1b52efc039db
Павел Николаев <pavelnikolaev1993@mail.ru>
2022-04-04 18:10:04 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Бананы",
+                ProdPrice = 70,
+                ProdImage = "Data/банан.png",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Кокосы",
+                ProdPrice = 270,
+                ProdImage = "Data/кокос.png",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Утюги",
+                ProdPrice = 370,
+                ProdImage = "Data/утюг.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Пылесосы",
+                ProdPrice = 470,
+                ProdImage = "Data/пылесос.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
